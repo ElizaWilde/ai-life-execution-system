e877b8327ef45340876b22057b726d2f5e2b9304
Eliza Wilde <qye9828@gmail.com>
2026-07-11 14:43:03 +0800

    commit __init__

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,3 +1,12 @@
+'''
+    __init__.py is a special Python file used to define a directory as a Python package.
+    1. Mark a directory as a package: With __init__.py, Python can treat the directory as an importable package
+    2. Run initialization code: Code inside __init__.py runs when the package is imported
+    3. Simplify imports
+    4. Control public exports with __all__: This indicates which names are intended to be publicly available from the package
+    It is completely valid for the file to be empty.Its presence alone can indicate that models is a Python package.
+    In most FastAPI projects, __init__.py is either empty or used to collect commonly imported models, schemas, routers, or services.
+'''
 from app.schemas.daily_task import (
     DailyPlanGenerateRequest,
     DailyPlanResponse,
